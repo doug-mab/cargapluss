ec369982c6a222d8de3566be706bed7e9f12af64
Douglas Brum <douglas.mateus2005@gmail.com>
2022-06-06 19:05:34 -0300

    Add storage controller

---
+++ src/controllers/storageController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { logger } from '../util/logger';
+
+class StorageController {
+  index(req: Request, res: Response) {
+    res.json({
+      data: 'hey',
+    });
+  }
+}
+
+export default new StorageController();
